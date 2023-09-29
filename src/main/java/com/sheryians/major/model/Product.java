15213f0e0b422dd524bf431f86c08d347ff72d04
Nishanth <nishanths8810@gmail.com>
2023-09-29 22:34:32 +0530

    second commit

---
+++ src/main/java/com/sheryians/major/model/Product.java
@@ -0,0 +1,26 @@
+package com.sheryians.major.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id" ,referencedColumnName = "category_id")
+    private Category category;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+
+
+}
