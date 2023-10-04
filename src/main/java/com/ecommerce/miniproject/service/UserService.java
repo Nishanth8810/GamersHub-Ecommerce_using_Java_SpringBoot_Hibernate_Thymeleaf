dad23f1a8b0ceae565a8ad7de067419b1ed49a45
Nishanth <nishanths8810@gmail.com>
2023-10-04 23:50:17 +0530

    fourth_commit

---
+++ src/main/java/com/ecommerce/miniproject/service/UserService.java
@@ -0,0 +1,39 @@
+package com.ecommerce.miniproject.service;
+
+import com.ecommerce.miniproject.entity.User;
+import com.ecommerce.miniproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id){
+        return userRepository.findById(id);
+
+    }
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    public void removeUserById(int id){
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email)throws NoSuchElementException {
+        return userRepository.findUserByEmail(email);
+
+    }
+
+
+}
