75b641de187dd6dd099dbac14d419e4510b5c90a
Nishanth <nishanths8810@gmail.com>
2023-10-29 18:01:05 +0530

    code refactored

--- src/main/java/com/ecommerce/miniproject/controller/AdminProductController.java
+++ src/main/java/com/ecommerce/miniproject/controller/AdminProductController.java
@@ -90,85 +90,92 @@ public class AdminProductController {
 
     }
 
-
     @PostMapping("/admin/products/add")
     public String productAddPost(@Valid @ModelAttribute("productDTO") ProductDTO productDTO,
                                  BindingResult bindingResult,
                                  Model model,
-                                 @RequestParam("productImage") List<MultipartFile> fileList,
-                                 @RequestParam(name = "productColor", required = false) List<String> productColors
-                                ,@RequestParam(name = "productSize" ,required=false)List<String> productSizes)
+                                 @RequestPart("productImage") List<MultipartFile> fileList,
+                                 @RequestParam(name = "productColor", required = false) List<String> productColors,
+                                 @RequestParam(name = "productSize", required = false) List<String> productSizes) {
 
-            throws IOException {
-
-
-        if (bindingResult.hasErrors()) {
-            if (fileList.get(0).isEmpty()) {
-                model.addAttribute("errorProductImage", "add at least one image");
-                model.addAttribute("product", productDTO);
-                model.addAttribute("categories", categoryService.getAllCategory());
-                model.addAttribute("sizes", productSizeRepository.findAll());
-                model.addAttribute("colors", productColorRepository.findAll());
-                return "productsAdd";
-            }
-            model.addAttribute("sizes", productSizeRepository.findAll());
-            model.addAttribute("colors", productColorRepository.findAll());
-            model.addAttribute("productVariants", productVariantsService.getAllVariants());
-            model.addAttribute("categories", categoryService.getAllCategory());
-            return "productsAdd";
-        }
-
-        if (productService.getProductByName(productDTO.getName())) {
-            model.addAttribute("product", productDTO);
-            model.addAttribute("categories", categoryService.getAllCategory());
-            model.addAttribute("errorProduct", "Product with same name already exits");
+        if (bindingResult.hasErrors() || fileList.get(0).isEmpty()) {
+            handleValidationErrors(model, productDTO);
             return "productsAdd";
         }
         if (fileList.get(0).isEmpty()) {
-            model.addAttribute("errorProduct", "add at least one image");
-            model.addAttribute("product", productDTO);
-            model.addAttribute("categories", categoryService.getAllCategory());
+            model.addAttribute("errorProductImage",
+                    "Add at least one image");
+        }
+
+        Product product = createProductFromDTO(productDTO);
+
+        try {
+            productService.addProduct(product);
+            saveImagesAndProductVariants(product, fileList, productColors, productSizes);
+            return "redirect:/admin/products";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            model.addAttribute("errorProduct", "An error occurred while adding the product.");
             return "productsAdd";
         }
+    }
+
+    private void handleValidationErrors(Model model, ProductDTO productDTO) {
+        model.addAttribute("product", productDTO);
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("sizes", productSizeRepository.findAll());
+        model.addAttribute("colors", productColorRepository.findAll());
+    }
+
+    private Product createProductFromDTO(ProductDTO productDTO) {
         Product product = new Product();
         product.setId(productDTO.getId());
         product.setName(productDTO.getName());
-        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+        product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).orElse(null));
         product.setQuantity(productDTO.getQuantity());
         product.setPrice(productDTO.getPrice());
         product.setWeight(productDTO.getWeight());
         product.setDescription(productDTO.getDescription());
+        return product;
+    }
 
-        product.setImageName(fileList.get(0).getOriginalFilename());
-        productService.addProduct(product);
+    private void saveImagesAndProductVariants(Product product, List<MultipartFile> fileList,
+                                              List<String> productColors, List<String> productSizes) {
+
+
+
+        for (MultipartFile file : fileList) {
+            String imageUUID = file.getOriginalFilename();
+            Path fileNameAndPath = Paths.get(uploadDir, imageUUID);
+            try {
+                Files.write(fileNameAndPath, file.getBytes());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException();
+            }
+        }
 
         List<ProductVariants> productVariantsList = new ArrayList<>();
         if (productColors != null && !productColors.isEmpty()) {
             for (int i = 0; i < productColors.size(); i++) {
                 ProductVariants productVariants = new ProductVariants();
-                ProductColor productColor = productColorRepository.findById(Long.valueOf(productColors.get(i))).get();
-                ProductSize productSize = productSizeRepository.findById(Long.valueOf(productSizes.get(i))).get();
-                productVariants.setProductColor(productColor);
-                productVariants.setProductSize(productSize);
-                productVariants.setProduct(product);
-                productVariantsList.add(productVariants);
+                ProductColor productColor = productColorRepository.findById(Long.valueOf(productColors.get(i))).orElse(null);
+                ProductSize productSize = productSizeRepository.findById(Long.valueOf(productSizes.get(i))).orElse(null);
+                if (productColor != null && productSize != null) {
+                    productVariants.setProductColor(productColor);
+                    productVariants.setProductSize(productSize);
+                    productVariants.setProduct(product);
+                    productVariantsList.add(productVariants);
+                }
             }
-
-        }
-        else {
+        } else {
             ProductVariants productVariants = new ProductVariants();
             productVariants.setProduct(product);
             productVariantsList.add(productVariants);
         }
         product.setProductVariants(productVariantsList);
 
-        for (MultipartFile file : fileList) {
-            String imageUUID = file.getOriginalFilename();
-            Path fileNameAndPath = Paths.get(uploadDir, imageUUID);
-            Files.write(fileNameAndPath, file.getBytes());
-        }
         List<ProductImage> productImageList = new ArrayList<>();
-
         for (int i = 1; i < fileList.size(); i++) {
             ProductImage productImage = new ProductImage();
             productImage.setProduct(product);
@@ -177,9 +184,101 @@ public class AdminProductController {
             productImageList.add(productImage);
         }
         product.setProductImages(productImageList);
-        productService.addProduct(product);
-        return "redirect:/admin/products";
     }
+
+
+
+//    @PostMapping("/admin/products/add")
+//    public String productAddPost(@Valid @ModelAttribute("productDTO") ProductDTO productDTO,
+//                                 BindingResult bindingResult,
+//                                 Model model,
+//                                 @RequestParam("productImage") List<MultipartFile> fileList,
+//                                 @RequestParam(name = "productColor", required = false) List<String> productColors
+//                                ,@RequestParam(name = "productSize" ,required=false)List<String> productSizes)
+//
+//            throws IOException {
+//        if (bindingResult.hasErrors()) {
+//            if (fileList.get(0).isEmpty()) {
+//                model.addAttribute("errorProductImage", "add at least one image");
+//                model.addAttribute("product", productDTO);
+//                model.addAttribute("categories", categoryService.getAllCategory());
+//                model.addAttribute("sizes", productSizeRepository.findAll());
+//                model.addAttribute("colors", productColorRepository.findAll());
+//                return "productsAdd";
+//            }
+//            model.addAttribute("sizes", productSizeRepository.findAll());
+//            model.addAttribute("colors", productColorRepository.findAll());
+//            model.addAttribute("productVariants", productVariantsService.getAllVariants());
+//            model.addAttribute("categories", categoryService.getAllCategory());
+//            return "productsAdd";
+//        }
+//
+//        if (productService.getProductByName(productDTO.getName())) {
+//            model.addAttribute("product", productDTO);
+//            model.addAttribute("categories", categoryService.getAllCategory());
+//            model.addAttribute("errorProduct", "Product with same name already exits");
+//            return "productsAdd";
+//        }
+//        if (fileList.get(0).isEmpty()) {
+//            model.addAttribute("errorProduct", "add at least one image");
+//            model.addAttribute("product", productDTO);
+//            model.addAttribute("categories", categoryService.getAllCategory());
+//            return "productsAdd";
+//        }
+//        try {
+//
+//            Product product = new Product();
+//            product.setId(productDTO.getId());
+//            product.setName(productDTO.getName());
+//            product.setCategory(categoryService.getCategoryById(productDTO.getCategoryId()).get());
+//            product.setQuantity(productDTO.getQuantity());
+//            product.setPrice(productDTO.getPrice());
+//            product.setWeight(productDTO.getWeight());
+//            product.setDescription(productDTO.getDescription());
+//
+//            product.setImageName(fileList.get(0).getOriginalFilename());
+//            productService.addProduct(product);
+//
+//            List<ProductVariants> productVariantsList = new ArrayList<>();
+//            if (productColors != null && !productColors.isEmpty()) {
+//                for (int i = 0; i < productColors.size(); i++) {
+//                    ProductVariants productVariants = new ProductVariants();
+//                    ProductColor productColor = productColorRepository.findById(Long.valueOf(productColors.get(i))).get();
+//                    ProductSize productSize = productSizeRepository.findById(Long.valueOf(productSizes.get(i))).get();
+//                    productVariants.setProductColor(productColor);
+//                    productVariants.setProductSize(productSize);
+//                    productVariants.setProduct(product);
+//                    productVariantsList.add(productVariants);
+//                }
+//
+//            } else {
+//                ProductVariants productVariants = new ProductVariants();
+//                productVariants.setProduct(product);
+//                productVariantsList.add(productVariants);
+//            }
+//            product.setProductVariants(productVariantsList);
+//
+//            for (MultipartFile file : fileList) {
+//                String imageUUID = file.getOriginalFilename();
+//                Path fileNameAndPath = Paths.get(uploadDir, imageUUID);
+//                Files.write(fileNameAndPath, file.getBytes());
+//            }
+//            List<ProductImage> productImageList = new ArrayList<>();
+//
+//            for (int i = 1; i < fileList.size(); i++) {
+//                ProductImage productImage = new ProductImage();
+//                productImage.setProduct(product);
+//                productImage.setImageName(fileList.get(i).getOriginalFilename());
+//                productImageRepository.save(productImage);
+//                productImageList.add(productImage);
+//            }
+//            product.setProductImages(productImageList);
+//            productService.addProduct(product);
+//            return "redirect:/admin/products";
+//        }catch (Exception e){
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @GetMapping("/admin/product/update/{id}")
     public String updateProductGet(@PathVariable int id, Model model) {
